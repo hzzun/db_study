b508b0f6a318700e64a9222b392bfed8178098c2
문현준 <jann1653@gmail.com>
2025-04-08 01:07:33 +0900

    initial commit

---
+++ src/main/kotlin/com/db_study/DbStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.db_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<DbStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/db_study/DbStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.db_study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
