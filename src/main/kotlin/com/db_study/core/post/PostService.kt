bc4b641410de1da1f9c03daa0b4cd5e90e0f5082
문현준 <jann1653@gmail.com>
2025-04-10 00:18:15 +0900

    Post Append with Redis

--- src/main/kotlin/com/db_study/core/post/PostService.kt
+++ src/main/kotlin/com/db_study/core/post/PostService.kt
@@ -1,12 +1,21 @@
 package com.db_study.core.post
 
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 @Service
 class PostService(
-    private val postRepository: PostRepository
+    private val postRepository: PostRepository,
+    private val redisTemplate: RedisTemplate<String, Any>
 ) {
     fun append(content: String): Long {
+        // Redis 저장
+        val key = UUID.randomUUID().toString()
+        redisTemplate.opsForValue().set(key, content, 300, TimeUnit.SECONDS)
+
+        // 데이터베이스 저장
         return postRepository.save(content)
     }
 }
